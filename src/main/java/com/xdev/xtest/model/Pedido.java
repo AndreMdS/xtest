fc0bf47c395572ff1a836cb2df7b541fac8c3dc6
X <X@DARKDESK>
2018-06-12 07:11:09 -0300

    Commit Inicial

---
+++ src/main/java/com/xdev/xtest/model/Pedido.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.xdev.xtest.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author X
+ */
+public class Pedido implements Serializable {
+ 
+    private Integer Id;
+    private Integer clienteId;
+    private Integer valorTotal;
+    
+}
